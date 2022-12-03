e70ab35b6cbff9a464dfd97c713f174ba6086183
Jason Christopher <jason.christopher@u.nus.edu>
2022-12-03 22:53:37 +0800

    Add 2015 day 2

--- 2015/day2.py
+++ 2015/day2.py
@@ -1,12 +1,12 @@
 def main():
 
     dimensions = None
-    max_sum, curr_sum = 0, 0
 
     f = open("E:\Jason\Code\Advent-Of-Code\\2015\day2inputs.txt","r")
     dimensions = f.readlines()
 
     sums = 0
+    sums2 = 0
 
     for d in dimensions:
         dims = d.split("x")
@@ -22,7 +22,23 @@ def main():
         slack = min(a1, a2, a3)
         sums += 2 * (a1 + a2 + a3) + slack
     
-    print(sums)
+
+    for d in dimensions:
+        dims = d.split("x")
+
+        l = int(dims[0])
+        w = int(dims[1])
+        h = int(dims[2])
+
+        a1 = l + w
+        a2 = l + h
+        a3 = w + h
+
+        perimeter = 2 * min(a1, a2, a3)
+        volume = l * w * h
+        sums2 += perimeter + volume
+
+    print(sums2)
 
 if __name__ == '__main__':
     main()
